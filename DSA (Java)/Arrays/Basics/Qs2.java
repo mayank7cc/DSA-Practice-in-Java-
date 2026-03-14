bd0ce7c80f7ec9492069fc197cac75d3c8d9e006
Mayank Parab <mayankparab2006@gmail.com>
2026-03-14 16:22:25 +0530

    QsUpdated

--- DSA (Java)/Arrays/Basics/Qs2.java
+++ DSA (Java)/Arrays/Basics/Qs2.java
@@ -1,5 +1,5 @@
 //Find the Sum of All Elements.
-
+import java.util.*;
 public class Qs2 {
     public static void main(String[] args){
         int[] arr = {100,200,700,1000,3000};
@@ -12,5 +12,26 @@ public class Qs2 {
             sum+=arr[j];
         }
         System.out.println("Sum: " + sum);
+
+        //Using user inputs :
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an Array: ");
+        int n = sc.nextInt();
+        int[] arry = new int[n];
+        System.out.println("Enter " + n +  " elements :");
+        for( int x = 0 ; x < n ; x++){
+            arry[x] = sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for(int y = 0 ; y < arry.length ; y++){
+            System.out.println(arry[y]);
+        }
+        System.out.println("Sum of Elements:");
+        int Sum = 0;
+        for(int k = 0 ; k < arry.length ; k++){
+            Sum = Sum + arry[k];
+        }
+        System.out.println(Sum);
     }
 }
+
